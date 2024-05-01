f8834f3a26bf9231c4f12fec5b73457126e94184
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-05-01 17:39:21 +0700

    feat: add url projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
 import MaxWidthWrapper from "@/components/max-width-wrapper"
+import Link from "next/link"
 
 export default function Home() {
+  const projects = [
+    {
+      title: "counter",
+      hrefUrl: "/counter",
+    },
+    {
+      title: "todo list",
+      hrefUrl: "/todo-list",
+    },
+    {
+      title: "modal",
+      hrefUrl: "/modal",
+    },
+  ]
   return (
-    <MaxWidthWrapper>list use case project what i learn redux</MaxWidthWrapper>
+    <MaxWidthWrapper className="flex items-center flex-col my-20">
+      <h1 className="font-semibold text-5xl mb-20">
+        learn redux toolkit with study case ⬇️⬇️
+      </h1>
+      <div className="grid grid-cols-3 gap-6 w-full">
+        {projects?.map(({ title, hrefUrl }) => (
+          <Link
+            key={title}
+            href={hrefUrl}
+            className="p-4 transition-all rounded-2xl border w-full h-20 border-white hover:bg-white/10 text-white flex items-center justify-center"
+          >
+            {title}
+          </Link>
+        ))}
+      </div>
+    </MaxWidthWrapper>
   )
 }
