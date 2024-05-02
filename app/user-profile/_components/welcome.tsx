fbb6078d38804f3e338e31f4368725a8ea8ed5b5
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-05-02 19:10:20 +0700

    update render data

--- app/user-profile/_components/welcome.tsx
+++ app/user-profile/_components/welcome.tsx
@@ -4,11 +4,11 @@ import { RootState } from "@/redux/store"
 import { useSelector } from "react-redux"
 
 const Welcome = () => {
-  const profile = useSelector((state: RootState) => state.profile.user_profile)
-
-  const name = (
-    <p className="ml-2 text-yellow-500 capitalize">{profile.username}</p>
+  const username = useSelector(
+    (state: RootState) => state.profile.user_profile.username
   )
+
+  const name = <p className="ml-2 text-yellow-500 capitalize">{username}</p>
   return (
     <div className="p-4 text-xl rounded-md bg-green-500/20 border-2 border-dashed border-green-500/40 flex items-center">
       Welcome back {name} 👋
